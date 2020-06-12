8a169eca7d8f6b74d29527b67034c7f54d16e881
Mileba <mykellforbes@gmail.com>
2020-06-12 10:11:03 +0100

    Completed Batch

--- git-autopush-windows.bat
+++ git-autopush-windows.bat
@@ -7,4 +7,21 @@ cd<path>
 ::Initialize Github
 git init
 
+::Add all files in the directory
+git add .
 
+::Commit all changes with any message you like "commit message"
+git commit -m "commit message"
+
+::Push changes to Github
+git Push
+
+::Notify user about completionn
+echo Push Complete
+
+::Wait 5 minutes until starting the loop.
+::Change as you like
+TIMEOUT 300
+
+::Start loop
+goto loop
