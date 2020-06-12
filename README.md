8dbfadbeca8855a44ddd8da861414e3ec1c48856
Mileba <mykellforbes@gmail.com>
2020-06-12 09:49:20 +0100

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a Windows Batch code that let's you auto push your code to github,
+
---
+++ git-autopush-windows.bat
@@ -0,0 +1,10 @@
+:loop
+
+:: Move to the directory you are using
+:: Change <path> as needed. Eg. C:\Users\Mileba\Documents\Codes
+cd<path>
+
+::Initialize Github
+git init
+
+
